b72e7cd5eac648f6481dad85f919cbeaef469d6f
Atychis <ricardojimenez00759@gmail.com>
2024-11-25 22:26:47 -0500

    requirements.txt

--- api/main.py
+++ api/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import JSONResponse, RedirectResponse
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
+from mangum import Mangum
 import os
 
 from .audio_process import mel_spectrogram
@@ -21,9 +22,11 @@ app.add_middleware(
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
+handler = Mangum(app)
+
 @app.get("/")
-def hello_fast_api():
-    return "Hello!"
+async def hello_fast_api():
+    return {"message": "Hello! FastApi"}
 
 @app.get("/favicon.ico")
 async def favicon():
