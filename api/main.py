2802010803b234fb27eee9027132f60cf2610615
Atychis <ricardojimenez00759@gmail.com>
2025-04-19 01:18:06 -0500

    add origin

--- api/main.py
+++ api/main.py
@@ -13,6 +13,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "https://bird-app-nextjs-pwa.vercel.app",
+        "https://bird-app-nextjs-pwa-production.up.railway.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
